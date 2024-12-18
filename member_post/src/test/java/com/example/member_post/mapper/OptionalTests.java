7906cd04c5d4d847d4c103b0f193b7c8c3527f04
seunghwan <sounghan94@gmail.com>
2024-12-18 18:25:02 +0900

    'optional'

---
+++ member_post/src/test/java/com/example/member_post/mapper/OptionalTests.java
@@ -0,0 +1,21 @@
+package com.example.member_post.mapper;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class OptionalTests {
+  @Test
+  public void testOptional(){
+    Optional<String> o = Optional.ofNullable(null);
+    log.info(o);
+    log.info(o.isPresent());
+    log.info(o.get());
+  }
+
+}
